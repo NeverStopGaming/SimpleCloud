fbbf57dfc6a82383fa0ec3485705d672ad56394e
Panda260 <Panda260@NeverStopGaming.net>
2021-10-03 00:27:01 +0200

    add ClearConsoleCommand.kt

---
+++ simplecloud-base/src/main/kotlin/eu/thesimplecloud/base/manager/commands/ClearConsoleCommand.kt
@@ -0,0 +1,16 @@
+package eu.thesimplecloud.base.manager.commands
+
+import eu.thesimplecloud.api.command.ICommandSender
+import eu.thesimplecloud.launcher.console.command.CommandType
+import eu.thesimplecloud.launcher.console.command.annotations.Command
+import eu.thesimplecloud.launcher.console.command.annotations.CommandSubPath
+import eu.thesimplecloud.launcher.startup.Launcher
+
+@Command("clear", CommandType.CONSOLE, "cloud.command.clear")
+class ClearConsoleCommand {
+
+    @CommandSubPath("","Clear the Console")
+    fun handleList(commandSender: ICommandSender) {
+        Launcher.instance.clearConsole()
+    }
+}
